bbddb93dc723797ddaefc7518430f334f9e09a07
Morten-Neteland <morten.neteland@telenor.no>
2024-03-26 23:08:37 +0000

    some additions

--- intercept.c
+++ intercept.c
@@ -11,11 +11,11 @@ typedef int (*orig_open_f_type)(const char *pathname, int flags, ...);
 typedef ssize_t (*orig_write_f_type)(int fd, const void *buf, size_t count);
 typedef int (*orig_close_f_type)(int fd);
 typedef int (*orig_fclose_f_type)(int fd);
+typedef int (*fclose_func_t)(FILE *);
 
 // Function prototypes for the original functions
 int open(const char *pathname, int flags, ...);
 ssize_t write(int fd, const void *buf, size_t count);
-int close(int fd);
 int fclose(FILE *fd);
 
 // Function to log the intercepted calls
@@ -88,28 +88,51 @@ ssize_t write(int fd, const void *buf, size_t count)
 }
 
 // Function to intercept close() calls
-int close(int fd)
-{
-    orig_close_f_type orig_close;
-    orig_close = (orig_close_f_type)dlsym(RTLD_NEXT, "close");
-    int ret = orig_close(fd);
-    if (ret != -1)
-    {
-        printf("[Intercept] Close file descriptor: %d\n", fd);
-    }
-    return ret;
-}
+// int close(int fd)
+// {
+//     orig_close_f_type orig_close;
+//     orig_close = (orig_close_f_type)dlsym(RTLD_NEXT, "close");
+//     int ret = orig_close(fd);
+//     if (ret != -1)
+//     {
+//         printf("[Intercept] Close file descriptor: %d\n", fd);
+//     }
+//     return ret;
+// }
 
 // Function to intercept close() calls
-int fclose(FILE *fd)
+int fclose(FILE *stream)
 {
-    orig_fclose_f_type orig_fclose;
-    orig_fclose = (orig_fclose_f_type)dlsym(RTLD_NEXT, "fclose");
-    int ret = orig_fclose(fd);
-    if (ret != -1)
+    // Load the original fclose function using dlsym
+    fclose_func_t original_fclose = dlsym(RTLD_NEXT, "fclose");
+
+    // Open file for writing the copied content
+    FILE *copy_file = fopen("text2.txt", "a");
+    if (copy_file == NULL)
     {
-        printf("[F-Intercept] Close file descriptor: %d\n", fd);
+        fprintf(stderr, "Error opening text2.txt for writing.\n");
+        exit(EXIT_FAILURE);
     }
 
-    return ret;
+    // Backup current position of the stream
+    long original_position = ftell(stream);
+
+    // Set the file position indicator to the beginning of the file
+    rewind(stream);
+
+    // Copy contents of the stream
+    int c;
+    while ((c = fgetc(stream)) != EOF)
+    {
+        fputc(c, copy_file);
+    }
+
+    // Restore original file position
+    fseek(stream, original_position, SEEK_SET);
+
+    // Close the copied file
+    fclose(copy_file);
+
+    // Call the original fclose function
+    return original_fclose(stream);
 }
