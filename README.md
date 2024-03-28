5c0968cb7e02d9cb576cbb31e93a0e89b9ce02c1
Morten-Neteland <morten.neteland@telenor.no>
2024-03-28 23:04:50 +0000

    added QOL improvements to intercept.c

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This is a proof of concept repo for intercepting and debugging libc interactions
  - vscode tasks. and launch.json to build interceptor shared object, and attach gdb debugger to whatever programming language is to debug (java, csharp etc.)
  - various simple programming language tests
  - intercept.c file, the libc hooks are stored here, which are common for all programming languages utilizing libc.
+
+
+## Useful docs and Q&A
+ - https://catonmat.net/simple-ld-preload-tutorial-part-two
+ - [libeatmydata](https://github.com/stewartsmith/libeatmydata)
+ - [ebpf write syscall hook](https://github.com/vishen/bpf-writesnoop/tree/master)
--- intercept.c
+++ intercept.c
@@ -1,35 +1,95 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <dlfcn.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+#define ASSIGN_DLSYM_OR_DIE(name)			\
+        libc_##name = (libc_##name##_##t)(intptr_t)dlsym(RTLD_NEXT, #name);			\
+        if (!libc_##name)                       \
+        {                                       \
+                const char *dlerror_str = dlerror();                          \
+                fprintf(stderr, "init error for %s: %s\n", #name,\
+                        dlerror_str ? dlerror_str : "(null)");                \
+                _exit(1);                       \
+        }
+
+typedef int (*libc_open_t)(const char*, int, ...);
+typedef int (*libc_open64_t)(const char*, int, ...);
+
+/* All the following are thread-local, to avoid initialization races between
+ * threads. */
+static __thread libc_open_t libc_open= NULL;
+static __thread libc_open64_t libc_open64= NULL;
+
+static void preload_init() {
+    ASSIGN_DLSYM_OR_DIE(open);
+	ASSIGN_DLSYM_OR_DIE(open64);
+}
+
+int *target_fd;
+
+// Custom implementation of open
+int open64(const char *pathname, int flags, ...)
+{
+    if (!libc_open64) preload_init();
+
+    va_list argp;
+    va_start(argp, flags);
+    mode_t mode = va_arg(argp, mode_t);
+    va_end(argp);
+
+    const char str[17] = "filenamejava.txt";
+    if (strstr(pathname, str) != NULL)
+    {
+        printf("Intercepted open64: %s\n", pathname);
+    }
+
+	return (*libc_open64)(pathname,flags,mode);
+}
 
 // Custom implementation of open
 int open(const char *pathname, int flags, ...)
 {
-    int (*original_open)(const char *pathname, int flags, ...);
-    // Load the original open function from libc
-    original_open = dlsym(RTLD_NEXT, "open");
+   if (!libc_open) preload_init();
 
-    // Your interception logic here
-    // For example, you can print the file being opened
-    printf("Intercepted open operation: %s\n", pathname);
+    va_list argp;
+    va_start(argp, flags);
+    mode_t mode = va_arg(argp, mode_t);
+    va_end(argp);
 
-    return original_open(pathname, flags);
+    const char str[17] = "filenamejava.txt";
+    if (strstr(pathname, str) != NULL)
+    {
+        printf("Intercepted open: %s\n", pathname);
+    }
+
+	return (*libc_open64)(pathname,flags,mode);
 }
 
 // Custom implementation of open64
 // open64() function is a part of the large file extensions, and is equivalent to calling open() with the O_LARGEFILE flag.
-int open64(const char *pathname, int flags, ...)
-{
-    int (*original_open64)(const char *pathname, int flags, ...);
-    // Load the original open64 function from libc
-    original_open64 = dlsym(RTLD_NEXT, "open64");
+// int open64(const char *pathname, int flags, ...)
+// {
+//     int (*original_open64)(const char *pathname, int flags, ...);
+//     // Load the original open64 function from libc
+//     original_open64 = dlsym(RTLD_NEXT, "open64");
 
-    // Your interception logic here
-    // For example, you can print the file being opened
-    printf("Intercepted open64 operation: %s\n", pathname);
+//     // Your interception logic here
+//     // For example, you can print the file being opened
+//     printf("Intercepted open64 operation: %s\n", pathname);
 
-    return original_open64(pathname, flags);
-}
+//     const char str[17] = "filenamejava.txt";
+
+//     if (strstr(pathname, str) != NULL)
+//     {
+//         printf("efoiejwghew: %s\n", pathname);
+//     }
+
+//     return original_open64(pathname, flags);
+// }
 
 // Custom implementation of write
 // ssize_t write(int fd, const void *buf, size_t count)
